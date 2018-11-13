265c112e1b8ea92479c0b7bb6cced4cc1e70a57b
LuisCasas <sc.luis@gmail.com>
2018-11-13 22:17:47 +0000

    Start adding production server

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
 const path = require('path');
 const public_dir = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: public_dir,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_module/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: public_dir
-    }
+module.exports = () => {
+    return {
+        entry: './src/app.js',
+        output: {
+            path: public_dir,
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_module/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: public_dir
+        }
+    };
 };
